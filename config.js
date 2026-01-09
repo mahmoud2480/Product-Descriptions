376756387106b3e52e7268c9159a80b443610f85
mahmoud2480 <g.store2480@gmail.com>
2026-01-10 01:16:57 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // ملف إعدادات مشترك لكل الواجهات
-window.API_URL = "https://script.google.com/macros/s/AKfycbz0Q18Gw_BdkOBhKpWNg-ngaGiOnbCuuGwKthlJBgoUZX97uN83lbzSjYpkovs6FtRN/exec";
+window.API_URL = "https://script.google.com/macros/s/AKfycbzRrHmv5nVLapn2ywFogvHfN5Y83zZUx5SpRi1Dhka7MY-1Y07_WfPZf2yRb7hYTmyj/exec";
