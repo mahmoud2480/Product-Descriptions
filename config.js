4099c1190631a26d28b1d25d78e43203fef0bba8
mahmoud2480 <g.store2480@gmail.com>
2026-01-11 20:58:19 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // config.js
-window.API_URL = "https://script.google.com/macros/s/AKfycbyQWFCMABA0qw4hGL9QS476-u6-7mSVVgQcAA-TlHbZjyRoC_udOa24nWSonJK1x-k/exec";
+window.API_URL = "https://script.google.com/macros/s/AKfycbwWYNiNxqaOu7J9NSmGfT0qmihseYx_IUjnmILruZ-Ew8cRZg4oOIJQ8cafu2wvXLEe/exec";
